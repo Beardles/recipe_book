54d6bb8b27a16222c016e26102b36dc093d930cb
John Beadles <jbeardles@gmail.com>
2019-06-29 12:08:18 -0500

    Remove client (for now); swap to mongo.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as config from 'config';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: config.get('database.url'),
+      database: config.get('database.name'),
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
